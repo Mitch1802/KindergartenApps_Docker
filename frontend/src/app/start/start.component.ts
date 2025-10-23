327967fcfc3485cfb42a2370e89a69a9f63cca30
Mitch1802 <m.r.96@gmx.net>
2025-10-24 01:06:11 +0200

    fix(ui): rollen bugfix

--- frontend/src/app/start/start.component.ts
+++ frontend/src/app/start/start.component.ts
@@ -3,45 +3,27 @@ import { Component, OnInit, inject } from '@angular/core';
 import { GlobalDataService } from '../_service/global-data.service';
 import { HeaderComponent } from '../_template/header/header.component';
 import { MatCardModule } from '@angular/material/card';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-    selector: 'app-start',
-    templateUrl: './start.component.html',
-    styleUrls: ['./start.component.sass'],
-    imports: [
+  selector: 'app-start',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.sass'],
+  imports: [
     HeaderComponent,
     MatCardModule,
     RouterLink,
     MatIconModule
-]
+  ]
 })
 export class StartComponent implements OnInit {
   private globalDataService = inject(GlobalDataService);
+  private router = inject(Router);
 
-  username: string = '';   
+  username: string = '';
   breadcrumb: any = [];
-  visibleItems:any = [
-    {
-      "icon": "dashboard",
-      "modul": "Lotusplan",
-      "rolle": "ADMIN, LOTUSPLAN",
-      "routerlink": "/lotusplan"
-    },
-    {
-      "icon": "engineering",
-      "modul": "Benutzerverwaltung",
-      "rolle": "ADMIN",
-      "routerlink": "/benutzer"
-    },
-    {
-      "icon": "settings",
-      "modul": "Konfiguration",
-      "rolle": "ADMIN",
-      "routerlink": "/konfiguration"
-    }
-  ]; 
+  rollen: any = [];
 
   ngOnInit(): void {
     sessionStorage.setItem('PageNumber', '1');
@@ -49,5 +31,13 @@ export class StartComponent implements OnInit {
     sessionStorage.setItem('Page2', '');
     this.username = sessionStorage.getItem('Benutzername') || 'Gast';
     this.breadcrumb = this.globalDataService.ladeBreadcrumb();
+
+    this.rollen = sessionStorage.getItem('Rollen');
+    if (this.rollen != null) {
+      this.rollen = this.rollen.split(',');
+      this.breadcrumb = this.globalDataService.ladeBreadcrumb();
+    } else {
+      this.router.navigate(['/login']);
+    }
   }
 }
